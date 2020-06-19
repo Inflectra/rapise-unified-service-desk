92ee38bb1fa4a4455ac299731daf73e1c82af84d
dmarkovtsev <denis@inflectra.com>
2020-06-19 18:28:43 +0300

    Login/Close

--- USDFramework/Objects.js
+++ USDFramework/Objects.js
@@ -1,1 +1,367 @@
-var saved_script_objects={};
+var saved_script_objects={
+	"G_Change_Credentials": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;8644a469-b84a-4415-8b82-4af219702507]",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Text",
+		"version": 0,
+		"object_type": "UIALabel",
+		"object_flavor": "Label",
+		"object_name": "G_Change_Credentials",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Loading Unified Service Desk...",
+		"location": "Window/btnChangeCredLink/lblLoginCredLink"
+	},
+	"G_Display_List": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "CheckBox"
+						},
+						{
+							"property": "Name",
+							"value": "Display list of available organizations"
+						},
+						{
+							"property": "AutomationId",
+							"value": "cbAskforOrg"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;082bfd8d-b68e-406d-baff-e56150997651]",
+		"object_text": "",
+		"object_role": "param:object_flavor",
+		"object_class": "param:object_flavor",
+		"version": 0,
+		"object_type": "UIACheckBox",
+		"object_flavor": "CheckBox",
+		"object_name": "G_Display_List",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk Logon",
+		"location": "Unified Service Desk Logon/CrmLoginCtrl/cbAskforOrg"
+	},
+	"G_UserName": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;082bfd8d-b68e-406d-baff-e56150997651]",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Pane",
+		"version": 0,
+		"object_type": "UIAObject",
+		"object_flavor": "UIAutomation",
+		"object_name": "G_UserName",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk Logon",
+		"location": "Unified Service Desk Logon/CrmLoginCtrl/GbAdvanced/tbUserId/PART_ContentHost"
+	},
+	"G_Online_Region": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "ComboBox"
+						},
+						{
+							"property": "Name",
+							"value": "Online Region"
+						},
+						{
+							"property": "AutomationId",
+							"value": "ddlCrmOnlineRegions"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;082bfd8d-b68e-406d-baff-e56150997651]",
+		"object_text": "",
+		"object_role": "param:object_flavor",
+		"object_class": "param:object_flavor",
+		"version": 0,
+		"object_type": "UIAComboBox",
+		"object_flavor": "ComboBox",
+		"object_name": "G_Online_Region",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk Logon",
+		"location": "Unified Service Desk Logon/CrmLoginCtrl/GbAdvanced/ddlCrmOnlineRegions"
+	},
+	"G_Password": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;082bfd8d-b68e-406d-baff-e56150997651]",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Pane",
+		"version": 0,
+		"object_type": "UIAObject",
+		"object_flavor": "UIAutomation",
+		"object_name": "G_Password",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk Logon",
+		"location": "Unified Service Desk Logon/CrmLoginCtrl/GbAdvanced/tbPassword/PART_ContentHost"
+	},
+	"G_Login": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Text"
+						},
+						{
+							"property": "Name",
+							"value": "_Login"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;082bfd8d-b68e-406d-baff-e56150997651]",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Text",
+		"version": 0,
+		"object_type": "UIALabel",
+		"object_flavor": "Label",
+		"object_name": "G_Login",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk Logon",
+		"location": "Unified Service Desk Logon/CrmLoginCtrl/btn_Connect/_Login"
+	},
+	"G_Organization": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;f7b7d7f8-5c09-44aa-b228-996ebef7b698]",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Text",
+		"version": 0,
+		"object_type": "UIALabel",
+		"object_flavor": "Label",
+		"object_name": "organization",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk Logon",
+		"location": "byname"
+	},
+	"G_Login1": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;f7b7d7f8-5c09-44aa-b228-996ebef7b698]",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Text",
+		"version": 0,
+		"object_type": "UIALabel",
+		"object_flavor": "Label",
+		"object_name": "G_Login",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk Logon",
+		"location": "Unified Service Desk Logon/CrmLoginCtrl/btnConnectOrg/_Login"
+	},
+	"G_DASHBOARD": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;21ea7fe3-4b38-435a-a5a2-8fa146d9c459]",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Text",
+		"version": 0,
+		"object_type": "UIALabel",
+		"object_flavor": "Label",
+		"object_name": "G_DASHBOARD",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk for Microsoft Dynamics 365",
+		"location": "MainAgentDesktop/MainWorkArea/Custom Panel/Group/ToolbarPanel/Toolbar Container/Group/Main/Dashboard/DASHBOARD"
+	},
+	"G_YesClose": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Button"
+						},
+						{
+							"property": "Name",
+							"value": "Yes"
+						},
+						{
+							"property": "AutomationId",
+							"value": "SecondButton"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:.*HwndWrapper\\[.+\\]"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:.*HwndWrapper\\[.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;sta_CustomDialogBox_thread;a5cbaf0e-f13e-4993-8491-e89290574ea3]",
+		"object_text": "",
+		"object_role": "param:object_flavor",
+		"object_class": "param:object_flavor",
+		"version": 0,
+		"object_type": "UIAButton",
+		"object_flavor": "Button",
+		"object_name": "G_YesClose",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk",
+		"location": "Unified Service Desk/Custom/SecondButton"
+	},
+	"G_Close": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Button"
+						},
+						{
+							"property": "Name",
+							"value": "Close"
+						},
+						{
+							"property": "AutomationId",
+							"value": "Close"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;152eeeae-438c-4b8e-93b3-2ea366e9f467]",
+		"object_text": "",
+		"object_role": "param:object_flavor",
+		"object_class": "param:object_flavor",
+		"version": 0,
+		"object_type": "UIAButton",
+		"object_flavor": "Button",
+		"object_name": "G_Close",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk for Microsoft Dynamics 365",
+		"location": "MainAgentDesktop/TitleBar/Close"
+	}
+};
--- USDFramework/User.js
+++ USDFramework/User.js
@@ -1,6 +1,9 @@
 //Put your custom functions and variables in this file
 
-g_browserLibrary = "UnifiedServiceDesk_IE";
+g_browserLibrary = "UnifiedServiceDesk_IE";   // Set to one of "UnifiedServiceDesk_IE" or "Selenium - Chrome"
+
+g_usdConfigPath = "ConfigInflectraIE.xlsx";
+
 g_recordUrls = false;
 
 if (!g_recording)
@@ -34,14 +37,76 @@ function USDLaunch()
 	{
 		Tester.Message("Unified Service Desk Client is not started.");
 		Global.DoLaunch(usdPath);
+		if (Global.DoWaitFor("G_Change_Credentials"))
+		{
+			SeS('G_Change_Credentials').DoClick();
+			USDLogin();
+		}
 		zWaitForMainUSDWindow();
 		return;
 	}
 }
 
+function USDLogin()
+{
+	var region = Global.GetProperty('OnlineRegion', "", "%WORKDIR%/" + g_usdConfigPath);
+	var usr = Global.GetProperty('UserName', "", "%WORKDIR%/" + g_usdConfigPath);
+	var pwd = Global.GetProperty('Password', "", "%WORKDIR%/" + g_usdConfigPath);
+	var org = Global.GetProperty('OrganizationName', "", "%WORKDIR%/" + g_usdConfigPath);
+	
+	var commandInterval = g_commandInterval;
+	g_commandInterval = 100;
+	
+	SeS('G_Display_List').DoSetCheck(true);
+	
+	var regionCombo = SeS('G_Online_Region');
+	regionCombo.DoClick(regionCombo.GetWidth() - 10);
+	regionCombo.DoClick(regionCombo.GetWidth() - 10);
+	regionCombo.DoSelectItem(region);
+	
+	SeS('G_UserName').DoClick();
+	SeS('G_UserName').DoSendKeys("^a");
+	SeS('G_UserName').DoSendKeys(usr);
+	SeS('G_Password').DoClick();
+	SeS('G_Password').DoSendKeys("^a");
+	SeS('G_Password')._DoSendKeys(pwd);
+	Global.DoSleep(500);
+	SeS('G_Login').DoClick();
+	
+	Global.DoSleep(3000);
+	SeS('G_Organization', {object_name: org}).DoClick();
+	SeS('G_Login1').DoClick();
+	
+	g_commandInterval = commandInterval;
+	
+	Global.DoWaitFor("G_DASHBOARD", 300000);
+}
+
 function USDClose()
 {
+	//zCloseWindowsByTitle("regex:Unified Service Desk.*");
+	SeS('G_Close').DoClick();
+	if (Global.DoWaitFor("G_YesClose", 3000))
+	{
+		SeS('G_YesClose').DoClick();
+	}
+}
 
+function zCloseWindowsByTitle(regexTitle)
+{
+	var arrFoundWindows = g_util.FindWindows(regexTitle, 'regex:.*');
+	
+	for( var i=0;i<arrFoundWindows.length; i++)
+	{
+		var wnd = /**HWNDWrapper*/ arrFoundWindows[i];
+		
+		Log("Closing: "+wnd.Text);
+		
+		// This will close main window of the window's process.
+		wnd.CloseMainWindow();
+		
+		// Alternative way would be bringing window to front and sending Alt+F4 to it.
+	}
 }
 
 function zWaitForMainUSDWindow()
