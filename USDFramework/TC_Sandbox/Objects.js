b634d3133b6b2ee0c256066028756393307cad77
dmarkovtsev <denis@inflectra.com>
2020-06-23 17:52:50 +0300

    Sandbox for checking things

---
+++ USDFramework/TC_Sandbox/Objects.js
@@ -0,0 +1,211 @@
+var saved_script_objects={
+	"MY_WORK": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;7dba7a78-2235-4bbd-a164-4246db2ec835]",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Text",
+		"version": 0,
+		"object_type": "UIALabel",
+		"object_flavor": "Label",
+		"object_name": "MY WORK",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk for Microsoft Dynamics 365",
+		"location": "MainAgentDesktop/MainWorkArea/Custom Panel/Group/ToolbarPanel/Toolbar Container/Group/Main/My Work/MY WORK"
+	},
+	"SEARCH": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;7dba7a78-2235-4bbd-a164-4246db2ec835]",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Text",
+		"version": 0,
+		"object_type": "UIALabel",
+		"object_flavor": "Label",
+		"object_name": "SEARCH",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk for Microsoft Dynamics 365",
+		"location": "MainAgentDesktop/MainWorkArea/Custom Panel/Group/ToolbarPanel/Toolbar Container/Group/Main/Search/SEARCH"
+	},
+	"REMINDER": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;7dba7a78-2235-4bbd-a164-4246db2ec835]",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Text",
+		"version": 0,
+		"object_type": "UIALabel",
+		"object_flavor": "Label",
+		"object_name": "REMINDER",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk for Microsoft Dynamics 365",
+		"location": "MainAgentDesktop/MainWorkArea/Custom Panel/Group/ToolbarPanel/Toolbar Container/Group/Main/Reminder/REMINDER"
+	},
+	"r": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Text"
+						},
+						{
+							"property": "Name",
+							"value": "r"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;701c43af-938e-4021-aa2b-fc6eb5e76219]",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Text",
+		"version": 0,
+		"object_type": "UIALabel",
+		"object_flavor": "Label",
+		"object_name": "r",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Activity Reminder",
+		"location": "Activity Reminder/Custom/ControlCloseButton/r"
+	},
+	"APPLICATIONS": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;7dba7a78-2235-4bbd-a164-4246db2ec835]",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Text",
+		"version": 0,
+		"object_type": "UIALabel",
+		"object_flavor": "Label",
+		"object_name": "APPLICATIONS",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk for Microsoft Dynamics 365",
+		"location": "MainAgentDesktop/MainWorkArea/Custom Panel/Group/ToolbarPanel/Toolbar Container/Group/Main/Applications/APPLICATIONS"
+	},
+	"Locate_the_Customer": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;7dba7a78-2235-4bbd-a164-4246db2ec835]",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Text",
+		"version": 0,
+		"object_type": "UIALabel",
+		"object_flavor": "Label",
+		"object_name": "Locate the Customer",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk for Microsoft Dynamics 365",
+		"location": "MainAgentDesktop/Window/Menu/Locate Customer/Locate the Customer"
+	},
+	"ExpanderToggle": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;7dba7a78-2235-4bbd-a164-4246db2ec835]",
+		"object_text": "",
+		"object_role": "param:object_flavor",
+		"object_class": "param:object_flavor",
+		"version": 0,
+		"object_type": "UIAButton",
+		"object_flavor": "Button",
+		"object_name": "ExpanderToggle",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk for Microsoft Dynamics 365",
+		"location": "MainAgentDesktop/MainWorkArea/Custom Panel/Group/ExpanderSessionDetails/HeaderSite"
+	},
+	"ExpanderToggle1": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:HwndWrapper\\[UnifiedServiceDesk\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "HwndWrapper[UnifiedServiceDesk.exe;;7dba7a78-2235-4bbd-a164-4246db2ec835]",
+		"object_text": "",
+		"object_role": "param:object_flavor",
+		"object_class": "param:object_flavor",
+		"version": 0,
+		"object_type": "UIAButton",
+		"object_flavor": "Button",
+		"object_name": "ExpanderToggle",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "Unified Service Desk for Microsoft Dynamics 365",
+		"location": "MainAgentDesktop/MainWorkArea/Custom Panel/Group/RightPanelExpander/HeaderSite"
+	}
+};
